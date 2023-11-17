446ed6866c59ab1b75c47727d82420f201d98e33
Ваня <7328652@gmail.com>
2023-11-17 20:00:55 +0200

    first commit

---
+++ src/4.ts
@@ -0,0 +1,55 @@
+class Key {
+  constructor(private signature: number) {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public comeIn(person: Person): void {
+    if (this.tenants) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract OpenDoor(key: Key): boolean;
+}
+class MyHouse extends House {
+  public OpenDoor(key: Key): boolean {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      return true;
+    }
+    return false;
+  }
+}
+
+const key = new Key(5);
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.OpenDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
